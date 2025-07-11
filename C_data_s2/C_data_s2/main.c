7abb776d5a2c8f7ffd9aac1746477f9356cf9868
bdkleo <mizuo6188@gmail.com>
2025-07-11 20:16:24 +0800

    顺序表的经典算法

---
+++ C_data_s2/C_data_s2/main.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+//顺序表的经典算法
+
+//27-移除元素(力扣)
+//#include<stdio.h>
+//
+////双指针法
+//int removeElement(int* nums, int numsSize, int val)
+//{
+//	int src, dst;
+//	src = dst = 0;
+//	while (src < numsSize)
+//	{
+//		if (nums[src] == val)
+//		{
+//			src++;
+//		}
+//		else
+//		{
+//			nums[dst++] = nums[src++];
+//		}
+//	}
+//	return dst;
+//}
+
+
+//88-合并两个有序数组
+#include<stdio.h>
